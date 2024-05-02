cce28f8e3fe4e639eb20aa14c5371a062df7284a
tomas nieto rossi <tomasnieto249@gmail.com>
2024-05-02 11:04:05 -0300

    ejercicio 3 listo

--- simulacroevaluacion.sql
+++ simulacroevaluacion.sql
@@ -2,3 +2,6 @@
 ORDER by title asc
 2-select DISTINCT rating FROM film
 ORDER by rating asc
+3-select DISTINCT rating , replacement_cost FROM film
+WHERE rating like "pg%"
+ORDER by replacement_cost asc
