55881402c493e1a5a6e98a4c97f7f3a266dca10b
tomas nieto rossi <tomasnieto249@gmail.com>
2024-05-02 11:16:31 -0300

    ejercicio 4 listo

--- simulacroevaluacion.sql
+++ simulacroevaluacion.sql
@@ -5,3 +5,6 @@ ORDER by rating asc
 3-select DISTINCT rating , replacement_cost FROM film
 WHERE rating like "pg%"
 ORDER by replacement_cost asc
+4-select DISTINCT city,p.country_id FROM city c
+join country p on c.country_id = p.country_id 
+group by country
