96a90eccf8385f49e682a5390dc1f3eb67f4adb3
tomas nieto <tomasnieto249@gmail.com>
2024-05-22 11:38:42 -0300

    ej 2 listo

--- evaluacion5D.sql
+++ evaluacion5D.sql
@@ -1,3 +1,8 @@
 1-select DISTINCT * from crime_scene_report
  where city = "SQL City"
 ORDER by description ASC
+
+2-SELECT DISTINCT name,plate_number,car_make,car_model FROM drivers_license d
+join person p on d.id = p.license_id
+WHERE gender = "male" and age <40
+ORDER by name ASC
