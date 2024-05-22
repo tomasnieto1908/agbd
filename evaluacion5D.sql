de2c9e75ab1100ec335c0f7433bd3f5a4d112171
tomas nieto <tomasnieto249@gmail.com>
2024-05-22 11:44:27 -0300

    ej 4 listo

--- evaluacion5D.sql
+++ evaluacion5D.sql
@@ -6,3 +6,7 @@ ORDER by description ASC
 join person p on d.id = p.license_id
 WHERE gender = "male" and age <40
 ORDER by name ASC
+
+4-select name,address_street_name,address_number from person
+where address_street_name =  "Franklin Ave"
+order by address_number desc LIMIT 1 OFFSET 2
