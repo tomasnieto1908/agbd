33c4ab80e7babf34a5e29c4deb1ebbacf46fb264
tomas nieto <tomasnieto249@gmail.com>
2024-05-22 11:54:13 -0300

    ej 5 listo

--- evaluacion5D.sql
+++ evaluacion5D.sql
@@ -4,9 +4,13 @@ ORDER by description ASC
 
 2-SELECT DISTINCT name,plate_number,car_make,car_model FROM drivers_license d
 join person p on d.id = p.license_id
-WHERE gender = "male" and age <40
+WHERE gender = "female" and age <40
 ORDER by name ASC
 
 4-select name,address_street_name,address_number from person
 where address_street_name =  "Franklin Ave"
 order by address_number desc LIMIT 1 OFFSET 2
+
+5-SELECT DISTINCT min(age),max(age),avg(age),gender FROM drivers_license d
+join person p on d.id = p.license_id
+WHERE gender = "male"
